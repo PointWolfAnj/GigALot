855b481eebbb6b6aaf89241811432c0c9b46b6ae
PointWolfAnj <anojan31@gmail.com>
2022-06-30 16:39:50 +0100

    user route working

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-//const userRoute = require("./routes/users")
+const userRoute = require("./routes/users")
 
 dotenv.config();
 
@@ -17,16 +17,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-//app.use("/api/users", userRoute);
-
-app.get("/",(req,res)=>{
-    res.send("welcome to homepage")
-})
-
-/* app.get("/users",(req,res)=>{
-    res.send("welcome to user page")
-})
+app.use("/api/users", userRoute);
 
 app.listen(8800,()=>{
-    console.log("Backend sever is running!")
-}) */ 
+    console.log("Backend server is running!")
+}) 
