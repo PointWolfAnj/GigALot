cb982b8d73e0c93a9dd7504fec9c755b83acc3fe
PointWolfAnj <anojan31@gmail.com>
2022-06-30 15:28:47 +0100

    testing

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,19 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true},()=>{
+    console.log("Connect to MongoDB")
+});
+
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req,res)=>{
+    res.send("welcome to homepage")
+})
+
 app.listen(8800,()=>{
-    console.log("Backend sever is running! ")
+    console.log("Backend sever is running!")
 })
