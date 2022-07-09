91f794aa2aa552c9cdb6d13c9a97a3ce523e74ce
PointWolfAnj <anojan31@gmail.com>
2022-07-09 13:48:55 +0100

    issue with database connecting

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/",(req,res)=>{
-    res.send("hey its auth route")
-})
+//register
+router.post("/register", async (req, res) => {
+    const user = new User({
+        username:"Anojan",
+        email:"anojan@mail.com",
+        password:"1234"
+    })
+
+    await user.save();
+    res.send("ok")
+});
 
 module.exports = router
